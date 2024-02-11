908d95ce9a0365d14cda90f611ee64a9fadb60eb
Mirek Rup <miroslawrup7@gmail.com>
2024-02-11 21:12:53 +0100

    FAQ Contact Form (not finish)

--- src/js/main.js
+++ src/js/main.js
@@ -58,3 +58,104 @@ if (accordionsLoc.length) {
         })
     })
 }
+
+// contact form ----------------------------------------
+// const inputWrapperLoc = document.querySelectorAll(".input-wrapper, .textarea-wrapper");
+const inputWrapperLoc = document.querySelectorAll(".input-wrapper");
+
+inputWrapperLoc.forEach((elem) => {
+
+    elem.querySelector("input, textarea").value = "";
+
+    elem.addEventListener("click", () => {
+        elem.querySelector("label").classList.add("mini");
+        elem.querySelector(".error").classList.add("mini");
+        // elem.querySelector("input, textarea").classList.add("active");
+        elem.querySelector("input").classList.add("active");
+    })
+
+    // elem.querySelector("input, textarea").addEventListener("blur", (event) => {
+    elem.querySelector("input").addEventListener("blur", (event) => {
+        if (!event.target.value) {
+            elem.querySelector("label").classList.remove("mini");
+            elem.querySelector(".error").classList.remove("mini");
+            // elem.querySelector("input, textarea").classList.remove("active");
+            elem.querySelector("input").classList.remove("active");
+        }
+    })
+})
+
+// contact form validation ----------------------------------------
+const nameLoc = document.querySelector("#name");
+const mailLoc = document.querySelector("#mail");
+const messageLoc = document.querySelector("#message");
+
+const buttonLoc = document.querySelector(".send-form");
+
+let validationPass = true;
+
+const validateEmpty = (e, elem) => {
+    if (elem === undefined) {
+        elem = e.target;
+    }
+    if (!elem.value) {
+        elem.previousElementSibling.querySelector("span").innerText = "[ to pole jest wymagane ]";
+        validationPass = false;
+        elem.classList.add("error");
+    } else {
+        elem.previousElementSibling.querySelector("span").innerText = "";
+        elem.classList.remove("error");
+    }
+};
+
+const validateEmail = (e, elem) => {
+    mailLoc.addEventListener("keyup", validateEmail);
+    mailLoc.addEventListener("input", validateEmail);
+    if (elem === undefined) {
+        elem = e.target;
+    }
+    if (elem.value) {
+        
+        if (
+            !String(elem.value)
+                .toLowerCase()
+                .match(
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                )
+        ) {
+            elem.previousElementSibling.querySelector("span").innerText = " [ nieprawidłowy adres e-mail ]";
+            validationPass = false;
+            mailLoc.classList.add("error");
+        } else {
+            elem.previousElementSibling.querySelector("span").innerText = "";
+            mailLoc.classList.remove("error");
+        }
+    }
+};
+
+const validateAll = () => {
+    validationPass = true;
+    validateEmpty(undefined, document.querySelector("#name"))
+    validateEmpty(undefined, document.querySelector("#mail"))
+    validateEmpty(undefined, document.querySelector("#message"))
+    validateEmail(undefined, document.querySelector("#mail"))
+
+    if (validationPass) {
+        alert("Walidacja prawidłowa! :)");
+    } else {
+        alert("Walidacja nieprawidłowa! :(");
+    }
+}
+
+nameLoc.addEventListener("blur", validateEmpty);
+nameLoc.addEventListener("keyup", validateEmpty);
+
+mailLoc.addEventListener("blur", validateEmpty);
+mailLoc.addEventListener("keyup", validateEmpty);
+mailLoc.addEventListener("blur", validateEmail);
+mailLoc.addEventListener("change", validateEmail);
+
+messageLoc.addEventListener("blur", validateEmpty);
+messageLoc.addEventListener("keyup", validateEmpty);
+
+buttonLoc.addEventListener("click", validateAll)
